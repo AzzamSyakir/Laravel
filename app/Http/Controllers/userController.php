5c5512dd5213948fa6afcfacad08b2bfff310640
Fuckyou <94348484+phuby@users.noreply.github.com>
2023-01-30 21:53:56 +0700

    Initial commit

---
+++ app/Http/Controllers/userController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class userController extends Controller
+{
+    //    public function register(Request $request)
+    public function register(Request $request)
+
+    {
+        //register user
+        $request->validate([
+            'name'=> 'required|string',
+            'email'=> 'required|string',
+            'password'=> 'required|string'
+        ]);
+        $user = new User([
+            'name' => $request->name,
+            'password' => Hash::make($request->password),
+            'email' =>$request->email,
+        ]);
+        
+        $user->save();
+        return response()->json(['message' => 'user registered']);
+}
+public function login(Request $request)
+{
+    $request->validate([
+        'email'=> 'required|string|email',
+        'password'=> 'required|string'
+    ]);
+    
+    $credentials = request(['email', 'password']);
+    
+    if (!Auth::attempt($credentials))
+        return response()->json([
+            'message' => 'Unauthorized'
+        ], 401);
+    
+    $user = $request->user();
+    $tokenResult = $user->createToken('Personal Access Token');
+    $token = $tokenResult->token;
+    
+    if ($request->remember_me)
+        $token->expires_at = Carbon::now()->addWeeks(1);
+    
+    $token->save();
+    
+    return response()->json([
+        'access_token' => $tokenResult->accessToken,
+        'token_type' => 'Bearer',
+        'expires_at' => Carbon::parse(
+            $tokenResult->token->expires_at
+        )->toDateTimeString()
+    ]);
+    
+    
+    }
+}
